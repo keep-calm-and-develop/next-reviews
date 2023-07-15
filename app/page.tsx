2f3d3a32c76f837f31c2c32d4bbe7640476ccf42
Aniket <aniket@nirmata.com>
2023-07-15 17:42:32 +0530

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 import Heading from '@/components/Heading';
+import Link from 'next/link';
 
 const HomePage: React.FC = () => {
     return (
         <>
             <Heading>Indie Gamer</Heading>
-            <p>
+            <p className='pb-3'>
                 Only the best indie games, reviewed for you!
             </p>
+            <div className='border rounded shadow bg-white hover:shadow-xl w-80 sm:w-full'>
+                <Link className='flex flex-col sm:flex-row' href='/reviews/hollow-knight'>
+                    <img
+                        src='/images/hollow-knight.jpg'
+                        alt=''
+                        width='320'
+                        height='180'
+                        className='rounded-t sm:rounded-l sm:rounded-r-none'
+                    />
+                    <h2 className='font-semibold font-orbitron py-1 text-center'>Hollow Knight</h2>
+                </Link>
+            </div>
         </>
     );
 }
